580cccc5f0a781228d287b45dbd6f96a4a490439
kkmonlee <kkmonlee@gmail.com>
2016-12-12 19:02:41 +0000

    Works with 2 players

--- sketch_dec06b/sketch_dec06b.ino
+++ sketch_dec06b/sketch_dec06b.ino
@@ -1,13 +1,18 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(0, INPUT);
+  pinMode(2, INPUT);
+  pinMode(9, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int read = analogRead(0);
-  Serial.println(read);
+  int reading0 = analogRead(2);
+  int reading1 = analogRead(9);
+
+  String sendStr = (String) reading0 + "|" + (String) reading1;
+  Serial.println(sendStr);
+  
   delay(50);
 
 }
