b6470270a744e1174e2a5093de46ac665e1551e1
kkmonlee <kkmonlee@gmail.com>
2016-12-16 13:42:18 +0000

    Added comments

--- sketch_dec12a.ino
+++ sketch_dec12a.ino
@@ -1,16 +1,23 @@
+// Master Arduino
+
 int byteRead;
 
 void setup() {
-  Serial.begin(9600);
-  delay(100);
-
+	// Reads from baud 9600
+	Serial.begin(9600);
+	delay(100);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  while(Serial.available()) {
-    byteRead = Serial.read();
-    Serial.println(byteRead);
-    delay(50);
-  }
+	// While baug 9600 is still open
+	while(Serial.available()) {	
+		// Read from serial (this will be whatever Slave Arduino is outputting)
+		byteRead = Serial.read();
+		// Print what Master Arduino saw back out to 9600
+		// So Processing can read it
+		Serial.println(byteRead);
+		// Delay can be adjusted. 50ms was suitable for this laptop
+		// Recommended 1-5ms.
+		delay(50);
+	}
 }
--- sketch_dec12b.ino
+++ sketch_dec12b.ino
@@ -1,18 +1,23 @@
+// Slave Arduino
+
 void setup() {
-  pinMode(2, INPUT); 
-  pinMode(1, INPUT);
-  Serial.begin(9600);
-  delay(100);
+	// Potentiometers should be connected in analog pins 1 and 2
+	pinMode(2, INPUT); 
+	pinMode(1, INPUT);
+	// Contacting master Arduino on baud 9600
+	Serial.begin(9600);
+	delay(100);
 
 }
 
 void loop() {
-  int reading1 = analogRead(2);
-  int reading2 = analogRead(1);
+	int reading1 = analogRead(2);
+	int reading2 = analogRead(1);
 
-  String reading = (String) reading1 + "|" + (String) reading2;
-  
-  Serial.println(reading);
-  delay(50);
+	// Concaternate reading from the 2 potentiometers into one string
+	String reading = (String) reading1 + "|" + (String) reading2;
 
+	// Print that string
+	Serial.println(reading);
+	delay(50);
 }
